bb13cbcfa97bd71cf49200d6b1b5e115805c2e2d
Anis Azrina <aazryna28@gmail.com>
2024-05-15 22:55:43 +0800

    product dah siap display semua

--- src/main/java/com/example/groupproject/controller/ProductController.java
+++ src/main/java/com/example/groupproject/controller/ProductController.java
@@ -1,16 +1,91 @@
 package com.example.groupproject.controller;
 
+import com.example.groupproject.dbconfig.DatabaseConfig;
+import com.example.groupproject.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Controller
 public class ProductController{
-    @GetMapping("/menu")
-    public String menu(Model model) {
-        // Add data to the model
-        return "menu"; // This will return the name of your HTML template (e.g., example.html)
 
+        @GetMapping("/menu")
+        public String menu(Model model) {
+
+            model.addAttribute("products", getProducts());
+            return "menu";
+        }
+        @GetMapping("/menu_drink")
+        public String drink(Model model) {
+            System.out.println(getProducts());
+            model.addAttribute("products", getProducts());
+            return "menu_drink";
+        }
+        @GetMapping("/menu_dessert")
+        public String desserts(Model model) {
+        System.out.println(getProducts());
+        model.addAttribute("products", getProducts());
+        return "menu_dessert";
     }
+
+        //view
+        public List<Product> getProducts() {
+            String jdbcUrl = DatabaseConfig.JDBC_URL;
+            String username = DatabaseConfig.USERNAME;
+            String password = DatabaseConfig.PASSWORD;
+
+            //Database table and column names
+            String tableName = "product";
+            String productImage = "product_image";
+            String productId = "product_id";
+            String productName= "product_name";
+            String productDesc = "product_description";
+            String productPrice = "product_price";
+            String productCat = "product_category";
+            String productQty = "product_quantity";
+            List<Product> products = new ArrayList<>();
+
+
+            try {
+                //Connect to MariaDB
+                Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+                //Create a statement object
+                Statement statement = connection.createStatement();
+
+                String sql = "SELECT " +productImage+ ", " + productId + ", " + productName + ", " + productDesc + ", " + productPrice + ", " + productCat + ", " + productQty + " FROM " + tableName;
+
+
+                try (ResultSet resultSet = statement.executeQuery(sql)) {
+                    while (resultSet.next()) {
+                        String image = resultSet.getString(productImage);
+                        int id = resultSet.getInt(productId);
+                        String name = resultSet.getString(productName);
+                        String desc = resultSet.getString(productDesc);
+                        double price = resultSet.getDouble(productPrice);
+                        String category = resultSet.getString(productCat);
+                        int qty = resultSet.getInt(productQty);
+                        //create Product object and add to list
+                        Product product = new Product(image, id, name, desc, price,category, qty);
+                        products.add(product);
+                    }
+                    return products;
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    // Handle database errors
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Handle database connection errors
+            }
+
+            // Return the name of your HTML template (e.g., menu.html)
+            return products;
+        }
 
 }
--- src/main/java/com/example/groupproject/model/Product.java
+++ src/main/java/com/example/groupproject/model/Product.java
@@ -2,16 +2,83 @@ package com.example.groupproject.model;
 
 public class Product {
     //declare variables
-
+    private String product_image;
+    private int product_id;
+    private String product_name;
+    private String product_description;
+    private double product_price;
+    private String product_category;
+    private int product_quantity;
 
 
     //constructor
+    public Product (String product_image, int product_id, String product_name, String product_description, double product_price, String product_category, int product_quantity) {
 
+        this.product_image = product_image;
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_description = product_description;
+        this.product_price = product_price;
+        this.product_category = product_category;
+        this.product_quantity = product_quantity;
+    }
 
+    // Getters and setters
 
+    public String getProduct_image() {
+        return product_image;
+    }
 
+    public void setProduct_image(String product_image) {
+        this.product_image = product_image;
+    }
 
+    public int getProduct_id() {
+        return product_id;
+    }
 
-    //getters and setters
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+
+    public double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(double product_price) {
+        this.product_price = product_price;
+    }
+
+    public String getProduct_category() {
+        return product_category;
+    }
+
+    public void setProduct_category(String product_category) {
+        this.product_category = product_category;
+    }
+
+    public int getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public void setProduct_quantity(int product_quantity) {
+        this.product_quantity = product_quantity;
+    }
 
 }
