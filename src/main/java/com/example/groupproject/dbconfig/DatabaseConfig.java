4746efb2b7619d164c2d539a4dcf55baabbeb559
Megat <dmegat14@gmail.com>
2024-05-15 09:17:21 +0800

    Add jdbc driver and db config

---
+++ src/main/java/com/example/groupproject/dbconfig/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.example.groupproject.dbconfig;
+
+public class DatabaseConfig {
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/coffeeshop";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+
+}
